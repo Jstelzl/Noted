802d21e359aa57bb66ec9c24d7a140e131b109ef
[josiah stelzl] <[josiahstelzl@gmail.com]>
2022-10-11 14:40:50 -0400

    worked on apiRouter dependencies

---
+++ routes/apiRoutes.js
@@ -0,0 +1,3 @@
+const fs = require('fs');
+const uuid = require('uuid');
+const router = require('express').Router();
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
 const express = require('express');
 const htmlRoutes = require('./routes/htmlRoutes');
 const apiRoutes = require('./routes/apiRoutes');
+const app = express();
+const PORT = process.env.PORT || 3001;
