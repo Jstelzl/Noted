d9151812cb5473c84889182b20f35eb2c304cc84
[josiah stelzl] <[josiahstelzl@gmail.com]>
2022-10-11 15:23:02 -0400

    PORT test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const htmlRoutes = require('./routes/htmlRoutes');
 const apiRoutes = require('./routes/apiRoutes');
 const app = express();
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -10,6 +10,6 @@ app.use(express.static('public'));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-app.listen(PORT, function() {
-    console.log('server is listening on https://localhost/' + PORT);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
