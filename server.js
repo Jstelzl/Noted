ccf330ecd5f2334fad162303064ebd96ea2395a7
[josiah stelzl] <[josiahstelzl@gmail.com]>
2022-10-11 15:18:05 -0400

    finished server.js

--- server.js
+++ server.js
@@ -3,3 +3,13 @@ const htmlRoutes = require('./routes/htmlRoutes');
 const apiRoutes = require('./routes/apiRoutes');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, function() {
+    console.log('server is listening on https://localhost/' + PORT);
+});
