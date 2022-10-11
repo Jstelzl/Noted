bcd8c990ac9363943fc4eca3f6ced502734c4ebb
[josiah stelzl] <[josiahstelzl@gmail.com]>
2022-10-11 14:47:31 -0400

    added post route for apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,18 @@
 const fs = require('fs');
-const uuid = require('uuid');
 const router = require('express').Router();
+const uuid = require('uuid');
 
 // API routes
 router.get('/notes', (req, res) => {
     const data = fs.readFileSync('./db/db.json');
     res.json(JSON.parse(data));
-})
+});
+
+router.post("/notes", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync('./db/db.json'));
+    const addNote = req.body;
+    addNote.id = uuid.v4();
+    notes.push(addNote);
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+    res.json(notes);
+});
